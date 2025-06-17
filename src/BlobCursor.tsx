a9a2f44c358f06d5e5280833121d21d333315232
MOHDSAIFGIT <mohdsaifb173.com>
2025-06-17 08:50:52 +0530

    Changes

--- src/BlobCursor.tsx
+++ src/BlobCursor.tsx
@@ -1,8 +1,7 @@
-// "use client";
+"use client";
 
 import React, { useRef, useEffect, useCallback } from "react";
 import gsap from "gsap";
-// import "./BlobCursor.css";
 
 export interface BlobCursorProps {
   blobType?: "circle" | "square";
